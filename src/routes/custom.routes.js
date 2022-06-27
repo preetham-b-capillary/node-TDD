b3425d99d2a14ea1edcea65b9f691f0b9a79369a
preetham.b <preetham.b@capillarytech.com>
2022-06-28 01:44:59 +0530

    chapter 2 setup

--- src/routes/custom.routes.js
+++ src/routes/custom.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const { getRandomJoke } = require("../helpers/myHelper");
 const calculator = require("../mapper/calculator");
+const { checkIfUserHasAccess } = require("./middlewares/user.middleware");
 const router = express.Router();
 
 router.get("/calculator", function (req, res) {
@@ -8,6 +10,27 @@ router.get("/calculator", function (req, res) {
   const valueTwo = Number(req.query.valueTwo);
   const result = calculator(operation, valueOne, valueTwo);
   res.send({ success: true, result: result });
+});
+
+
+/*
+  write test case for this route, 
+  axios should not make any network call here - mock 
+*/
+router.get('/getRandomJoke', async (req, res) => {
+  const result = await getRandomJoke();
+  res.send(result);
+});
+
+
+/* 
+  this route has as middleware which checks the access, write Integration test, 
+  while writing it bypass the middleware which checks user access,
+  bypass using mock, dont pass query param in route 
+*/
+router.get('/securedRandomJoke', checkIfUserHasAccess, async (req, res) => {
+  const result = await getRandomJoke();
+  res.send(result);
 });
 
 module.exports = router;
