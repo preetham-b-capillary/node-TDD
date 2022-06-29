fba8cdd8c3abfff996cd5b66f8c3d93f9ea359f1
hitesh-cap <106952452+hitesh-cap@users.noreply.github.com>
2022-06-29 15:18:55 +0530

    Update firsttestcase.test.js

--- firsttestcase.test.js
+++ firsttestcase.test.js
@@ -1,1 +1,46 @@
 it.todo("jest setup");
+const request=require("supertest");
+const app=require('./index');
+const axios=require('axios');
+jest.mock('axios');
+const calculator = require("./src/mapper/calculator");
+
+describe('calculator testing',()=>{
+    it("addition check",()=>{
+        expect(calculator('addition',1,2,3)).toBe(6);
+    })
+    it("subtraction check",()=>{
+        expect(calculator('subtraction',2,3)).toBe(-1);
+    })
+    it("multiplication check",()=>{
+        expect(calculator('multiplication',1,2,3)).toBe(6);
+    })
+    it("division check",()=>{
+        expect(calculator('division',1,2)).toBe(2);
+    })
+})
+
+describe('jokes API testing',()=>{
+    it("sucessfull API request", async()=>{
+        const res= await request(app).get("/test/getRandomJoke");
+        expect(res.statusCode).toBe(200);
+    })
+    it("getRandomJoke API check", async() => {
+        axios.mockImplementation(() => 
+            Promise.resolve({data:{ joke: "Hello World!", error: false}})
+        );
+        const res = await request(app).get("/test/getRandomJoke");
+        expect(res.text).toBe("Hello World!");
+    });
+    it("securedRandomJoke API check", async() => {
+        axios.mockImplementation(() => 
+            Promise.resolve({data:{ joke: "Hello World!", error: false}})
+        );
+        const res = await request(app).get("/test/securedRandomJoke").query({ isSuperUser: 1 });
+        expect(res.text).toBe("Hello World!");
+    });
+    it("sucessfull API request secured", async()=>{
+        const res= await request(app).get("/test/securedRandomJoke");
+        expect(res.statusCode).toBe(200);
+    })
+})
