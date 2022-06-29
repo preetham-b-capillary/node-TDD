d2465087a2643522218500ba631fb5f443f3facd
raza-99 <81908867+raza-99@users.noreply.github.com>
2022-06-30 01:23:42 +0530

    Add files via upload

--- firsttestcase.test.js
+++ firsttestcase.test.js
@@ -1,1 +1,34 @@
+const request = require("supertest");
+const app = require("./index");
+const axios = require("axios");
+jest.mock("axios");
+
 it.todo("jest setup");
+
+const samplejoke =
+  "Relationship Status: just tried to reach for my dog's paw and he pulled it away so I pretended I was reaching for the remote.";
+
+describe("JOKES TEST", () => {
+  it(" API Success", async () => {
+    const res = await request(app).get("/test/getRandomJoke");
+    expect(res.statusCode).toBe(200);
+  });
+
+  it("Random Joke", async () => {
+    axios.mockImplementation(() =>
+      Promise.resolve({ data: { joke: samplejoke, error: false } })
+    );
+    const res = await request(app).get("/test/getRandomJoke");
+    expect(res.text).toBe(samplejoke);
+  });
+
+  it(" Secured Random Joke", async () => {
+    axios.mockImplementation(() =>
+      Promise.resolve({ data: { joke: samplejoke, error: false } })
+    );
+    const res = await request(app)
+      .get("/test/securedRandomJoke")
+      .query({ isSuperUser: 1 });
+    expect(res.text).toBe(samplejoke);
+  });
+});
