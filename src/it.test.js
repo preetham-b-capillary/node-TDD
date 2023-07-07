8d2c5fb31cc05deabd1b9fb69f573566fe402a87
priya-capillary <priya.pai@capillarytech.com>
2023-07-07 13:08:19 +0530

    mocking

---
+++ src/it.test.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const calculator = require('./mapper/calculator')
+const CONSTANTS = require("./const");
+const router = express.Router()
+
+
+const calculatorSubtractionFunctionMock = jest.fn((input1,input2)=>{
+    return input1 - input2
+})
+
+const calculatorInvalidOperationMock = jest.fn(()=>{
+    return 'Invalid operation '
+})
+
+test('Should add 2 numbers for Addtion operation', () => {
+
+    //Mock request
+    const req = {
+        query :{
+            operation: CONSTANTS.CALCULATOR_OPERATIONS.ADDITION,
+            valueOne: 2,
+            valueTwo: 3
+        }
+    }
+
+    // res.send({ success: true, result: result });
+    const responseMock = jest.fn(({success, result})=>{})
+    //Mock response
+    const res = {
+        send : responseMock
+    }
+
+    jest.mock('./mapper/calculator')
+    const calculatorMock = jest.fn((operation, valueOne, valueTwo)=>{
+        return valueOne + valueTwo
+    })
+    
+    router.get('/calculator', (req, res) => {} )
+    // expect(calculator).toHaveBeenCalledWith(CONSTANTS.CALCULATOR_OPERATIONS.ADDITION, 2 ,3)
+    expect(calculatorMock(CONSTANTS.CALCULATOR_OPERATIONS.ADDITION, 2, 3)).toBe(5)
+})
