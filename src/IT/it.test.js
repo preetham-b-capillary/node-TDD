d0c9ac9dbfcd3df863e52ccdfc4079936e2a874d
priya-capillary <priya.pai@capillarytech.com>
2023-07-10 13:03:14 +0530

    IT using superTest

--- src/IT/it.test.js
+++ src/IT/it.test.js
@@ -1,105 +1,44 @@
-const express = require("express");
-const calculator = require('../mapper/calculator')
 const CONSTANTS = require("../const");
-const router = express.Router()
-const mockRouter = require('./mock.custom.routes')
+const router = require('../routes/custom.routes')
+//const {calculator} = require('../mapper/calculator')
+const request = require('supertest')
+const app = require('../../index')
 
-const calculatorSubtractionFunctionMock = jest.fn((input1,input2)=>{
-    return input1 - input2
+//------------------------- TC-1 -----------------------------
+
+test('Test addition functionality', async () => {
+    
+    const response = await request(app).get('/test/calculator').query({ operation: "addition", valueOne: 1, valueTwo: 2 })
+    expect(response.body.result).toBe(3)
+})
+//------------------------- TC-2 -----------------------------
+test('Test the Response status code', async () => {
+
+    const response = await request(app).get('/test/calculator')
+    expect(response.statusCode).toBe(200)
 })
 
-const calculatorInvalidOperationMock = jest.fn(()=>{
-    return 'Invalid operation '
-})
+//------------------------- TC-3 -----------------------------
+test('Testing router to call calculator', async () => {
 
-//----------------- TC-1 ----------------------------------------
-test('Should add 2 numbers for Addtion operation', () => {
+    // jest.mock('../mapper/calculator', ()=>{
+    //     calculator: jest.fn((operation, valueOne, valueTwo) => {
+    //         return 'mock return from calculator'
+    //     })
+    // })
 
-    //Mock request
-    const req = {
-        query :{
-            operation: CONSTANTS.CALCULATOR_OPERATIONS.ADDITION,
-            valueOne: 2,
-            valueTwo: 3
-        }
-    }
-
-    // res.send({ success: true, result: result });
-    const responseMock = jest.fn(({success, result})=>{})
-    //Mock response
-    const res = {
-        send : responseMock
-    }
-
-    jest.mock('../mapper/calculator', () => {
-        return {
-          calculator: jest.fn((operation, valueOne, valueTwo)=>{
-                return valueOne + valueTwo
-            })
-        }
-      });
-
-    router.get('/calculator', (req, res) => {} )
-    expect(calculator(CONSTANTS.CALCULATOR_OPERATIONS.ADDITION, 2, 3)).toBe(5)
-})
-
-//----------------- TC-2 ----------------------------------------
-test('Subtract 2 numbers', () => {
 
     jest.mock('../mapper/calculator')
-    const calculatorMock = jest.fn((operation, valueOne, valueTwo) => {
-                    return valueOne - valueTwo
-                })
-    //mock req
-    const req = {
-        query: {
-            operation: CONSTANTS.CALCULATOR_OPERATIONS.SUBTRACTION,
-            valueOne: 2,
-            valueTwo: 3
-        }
-    }
-
-    //mock res
-    const res = {
-        send: (success, result)=>{}
-    }
-
-
-    router.get('/calculator', (req, res) => {})
-
-    expect(calculatorMock(CONSTANTS.CALCULATOR_OPERATIONS.SUBTRACTION, 5, 5)).toBe(0)
-})
-
-//----------------- TC-3 ----------------------------------------
-test('The Calculator func is called with right parameteres', () => {
-
-    
-    jest.mock('../mapper/calculator', () => {
-        return  {
-            calculator: jest.fn((operation, valueOne, valueTwo) => {
-                return valueOne + valueTwo
-            })
-        }
-
+    const calculator = jest.fn((operation, valueOne, valueTwo) => {
+            return 'Mock return from calculator'
     })
-    
 
-    //mock req
-    const req = {
-        query: {
-            operation: CONSTANTS.CALCULATOR_OPERATIONS.ADDITION,
-            valueOne: 1,
-            valueTwo: 2
-        }
-    }
-
-    jest.mock('../routes/custom.routes')
-    
-    
-
-    mockRouter.get('/calculator', (req, res) => {})
-
-    // expect(calculator).toHaveBeenCalledWith('addition', 1, 2);
+    const response = await request(app).get('/test/calculator');
+    //console.log("=== response = ",response)
+    // expect(response.status).toBe(200)
     expect(calculator).toHaveBeenCalledTimes(1)
-    
 })
+    
+    
+
+    
