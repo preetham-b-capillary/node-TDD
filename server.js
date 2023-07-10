a5346fc2d80708cc1e31dbba36676457a04ed9fb
preetham.b <preetham.b@capillarytech.com>
2023-07-10 18:02:08 +0530

    master push

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const customApp = require('./index');
 
 const server = customApp.listen(process.env.APP_PORT || 3000, () => {
   console.log(`App is running at port ${server.address().port}`);
-<<<<<<< HEAD
 });
 
 //testing 2
-=======
-});
->>>>>>> hotfix/1.2.1
